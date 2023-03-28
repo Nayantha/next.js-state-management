f52eaed92070a39a8a400a3f20a2b809d9b2253e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-28 11:48:47 +0530

    import store components and SSRUserTable component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import UserTable from "@/components/UserTable";
-
+import SSRUserTable from "@/components/SSRUserTable";
+import { store } from "@/store"
+import { setStartupUser } from "@/store/searchSlice"
 export default async function Home() {
   const req = await fetch("http://localhost:3000/api/searchUser", {"cache": "no-store"});
   const data = await req.json();
+  store.dispatch(setStartupUser(data))
   return (
       <main>
-          <UserTable users={data}></UserTable>
+          <SSRUserTable/>
       </main>
   );
 }
