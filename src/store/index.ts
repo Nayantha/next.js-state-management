ecc10aa2dc296f7b74fd914860270d92d430a26a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-28 12:43:09 +0530

    add user search api and bnd it with middleware.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit"
 import searchReducer from "./searchSlice"
-
+import { userApi } from "./userApi"
 export const store = configureStore({
     reducer: {
         search: searchReducer,
+        userApi: userApi.reducer,
     },
+    middleware(getDefaultMiddleware){
+        return getDefaultMiddleware().concat(userApi.middleware)
+    }
 })
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = ReturnType<typeof store.dispatch>
