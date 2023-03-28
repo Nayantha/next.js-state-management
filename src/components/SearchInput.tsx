04c471f2973a29f82776acd54bc8d59931c2f146
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-28 12:55:08 +0530

    add use effect and change the user table data.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,13 +1,16 @@
 "use client"
-import { useDispatch, useSelector} from "react-redux"
-import type { TypedUseSelectorHook } from "react-redux"
-import { RootState, AppDispatch } from "@/store"
-import { setSearch } from "@/store/searchSlice"
+import type {TypedUseSelectorHook} from "react-redux"
+import {useDispatch, useSelector} from "react-redux"
+import {AppDispatch, RootState} from "@/store"
+import {setSearch} from "@/store/searchSlice"
 import UserTable from "@/components/UserTable"
 import {User} from "@/types";
+import {useEffect} from "react";
+import {userApi} from "@/store/userApi";
+
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-const SearchInput = () =>{
+const SearchInput = () => {
     const dispatch = useAppDispatch()
     const search = useAppSelector((state) => state.search.search)
     const startupUser = useAppSelector((state) => state.search.startupUser)
@@ -15,13 +18,17 @@ const SearchInput = () =>{
         (state) =>
             state.userApi.queries[`search("${search}`]?.data as User[]
     )
+    useEffect(() => {
+        // @ts-ignore
+        dispatch(userApi.endpoints.search.initiate(search))
+    }, [dispatch, search])
     return (
         <div>
             <input type="text" value={search}
                 // @ts-ignore
                    onChange={(e) => dispatch(setSearch(e.target.value))}
             />
-            <UserTable users={startupUser}></UserTable>
+            <UserTable users={search.length ? data ?? [] : startupUser}></UserTable>
         </div>
     )
 }
