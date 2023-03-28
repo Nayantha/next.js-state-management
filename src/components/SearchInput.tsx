2971c69022bea64d2bd73468fec80d7a43227a10
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-28 12:19:37 +0530

    import and add user table.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,18 +3,20 @@ import { useDispatch, useSelector} from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
 import { RootState, AppDispatch } from "@/store";
 import { setSearch } from "@/store/searchSlice";
+import UserTable from "@/components/UserTable";
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 const SearchInput = () =>{
     const dispatch = useAppDispatch()
     const search = useAppSelector((state) => state.search.search)
+    const startupUser = useAppSelector((state) => state.search.startupUser)
     return (
         <div>
             <input type="text" value={search}
                 // @ts-ignore
                    onChange={(e) => dispatch(setSearch(e.target.value))}
             />
-            <div>{search}</div>
+            <UserTable users={startupUser}></UserTable>
         </div>
     )
 }
