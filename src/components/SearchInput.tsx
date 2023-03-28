1d9e8544b6d6d9c3df2b0ef31b7acb6e0ef68704
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-28 12:47:11 +0530

    add method to get data from api query using input.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,15 +1,20 @@
 "use client"
-import { useDispatch, useSelector} from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux";
-import { RootState, AppDispatch } from "@/store";
-import { setSearch } from "@/store/searchSlice";
-import UserTable from "@/components/UserTable";
+import { useDispatch, useSelector} from "react-redux"
+import type { TypedUseSelectorHook } from "react-redux"
+import { RootState, AppDispatch } from "@/store"
+import { setSearch } from "@/store/searchSlice"
+import UserTable from "@/components/UserTable"
+import {User} from "@/types";
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 const SearchInput = () =>{
     const dispatch = useAppDispatch()
     const search = useAppSelector((state) => state.search.search)
     const startupUser = useAppSelector((state) => state.search.startupUser)
+    const data = useAppSelector(
+        (state) =>
+            state.userApi.queries[`search("${search}`]?.data as User[]
+    )
     return (
         <div>
             <input type="text" value={search}
