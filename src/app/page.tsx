14f495975159bea062542eddd5459032ef025e8a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-28 12:15:22 +0530

    wrap input with provider component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import SearchInput from "@/components/SearchInput";
 import { store } from "@/store"
 import { setStartupUser } from "@/store/searchSlice"
+import Providers from "@/components/Provider";
 export default async function Home() {
   const req = await fetch("http://localhost:3000/api/searchUser", {"cache": "no-store"});
   const data = await req.json();
   store.dispatch(setStartupUser(data))
   return (
       <main>
-          <SearchInput/>
+          <Providers><SearchInput/></Providers>
       </main>
   );
 }
