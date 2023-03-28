1c9774acd13146722830ea5f761a4c93ad7daddf
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-28 12:28:26 +0530

    implement preloader.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@ import SearchInput from "@/components/SearchInput";
 import { store } from "@/store"
 import { setStartupUser } from "@/store/searchSlice"
 import Providers from "@/components/Provider";
+import Preloader from "@/components/Preloader";
 export default async function Home() {
   const req = await fetch("http://localhost:3000/api/searchUser", {"cache": "no-store"});
   const data = await req.json();
   store.dispatch(setStartupUser(data))
   return (
       <main>
-          <Providers><SearchInput/></Providers>
+          <Preloader users={data}/>
+              <Providers>
+                  <SearchInput/>
+              </Providers>
       </main>
   );
 }
