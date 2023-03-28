b8a59d00ffdc703351fd82ba23ff5ab15f597a77
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-28 12:09:43 +0530

    add state logic to input field.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,19 @@
+"use client"
+import { useDispatch, useSelector} from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+import { RootState, AppDispatch } from "@/store";
+import { setSearch } from "@/store/searchSlice";
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 const SearchInput = () =>{
+    const dispatch = useAppDispatch()
+    const search = useAppSelector((state) => state.search.search)
     return (
         <div>
-            <input type="text"/>
+            <input type="text" value={search}
+                // @ts-ignore
+                   onChange={(e) => dispatch(setSearch(e.target.value))}
+            />
         </div>
     )
 }
